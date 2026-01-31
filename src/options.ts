d20de5a919947d770b18e1bfd2a6fb1953bee319
손봉균 <sbg0629@naver.com>
2026-01-31 17:59:29 +0900

    음

---
+++ src/options.ts
@@ -0,0 +1,10 @@
+class Options {
+  useSkills: boolean = true;
+  winningRank: number = 0;
+  autoRecording: boolean = true;
+  adminMode: boolean = false;
+  riggedWinner: string = '';
+}
+
+const options = new Options();
+export default options;
